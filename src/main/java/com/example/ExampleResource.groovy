3d3859b556ebe54c223a0074f5dff42bcf34707c
Brandon Vergara <brandon@makingdevs.com>
2021-07-07 10:24:32 -0500

    [master] add function to get list

--- src/main/java/com/example/ExampleResource.groovy
+++ src/main/java/com/example/ExampleResource.groovy
@@ -1,6 +1,8 @@
 package com.example
 
 import com.example.entity.Corresponsal
+import com.example.entity.Cotizador
+import com.example.entity.Plazo
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -10,19 +12,34 @@ import javax.ws.rs.core.MediaType
 @Path("/")
 class ExampleResource {
 
-    @GET
-    @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    String hello(){
-        "Hola mundo"
-    }
+  @GET
+  @Path("/hello")
+  @Produces(MediaType.TEXT_PLAIN)
+  String hello() {
+    "Hola mundo"
+  }
 
-    @GET
-    @Path("/corresponsalList")
-    @Produces(MediaType.APPLICATION_JSON)
-    List<Corresponsal> corresponsalList() {
-        List<Corresponsal> corresponsalList = Corresponsal.listAll()
-        corresponsalList
-    }
+  @GET
+  @Path("/corresponsalList")
+  @Produces(MediaType.APPLICATION_JSON)
+  List<Corresponsal> corresponsalList() {
+    List<Corresponsal> corresponsalList = Corresponsal.listAll()
+    corresponsalList
+  }
 
+  @GET
+  @Path("/cotizadorList")
+  @Produces(MediaType.APPLICATION_JSON)
+  List<Cotizador> cotizadorList() {
+    List<Cotizador> cotizadorList = Cotizador.listAll()
+    cotizadorList
+  }
+
+  @GET
+  @Path("/plazoList")
+  @Produces(MediaType.APPLICATION_JSON)
+  List<Plazo> plazoList() {
+    List<Plazo> plazoList = Plazo.listAll()
+    plazoList
+  }
 }
